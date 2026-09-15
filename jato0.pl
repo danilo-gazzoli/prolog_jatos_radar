21c19566359491cdbf8ec0405faa4f5ba076c0b1
Danilo <dmartinsgazzoli@gmail.com>
2026-09-15 17:21:47 -0300

    feat(predicado perigo): implementação dos predicados que: calcula distância entre dois pontos, verifica se têm um missíl ou adversário perto, e conclui se o jato está em perigo.

--- jato0.pl
+++ jato0.pl
@@ -23,22 +23,22 @@
 %%% Faça seu codigo a partir daqui, sendo necessario sempre ter o predicado:
 %%%% obter_controles([X,Y,ANGLE,SCORE,SPEED], ADVERSARIOS, MISSEIS, [FORWARD, REVERSE, LEFT, RIGHT, BOOM, MSG]) :- ...
 
-troca(0, 1).
-troca(1, 0).
+% troca(0, 1).
+% troca(1, 0).
 
 % [FORWARD, REVERSE, LEFT, RIGHT, BOOM, MSG]
-obter_controles(INFORMACAO, ADVERSARIOS, MISSEIS, CONTROLES) :-
-    INFORMACAO = [X, Y, ANGLE, SCORE, SPEED],
-    CONTROLES = [FORWARD, REVERSE, LEFT, RIGHT, BOOM, MSG],
-    random_between(0,1,AA),
-    troca(AA, BB),
-    random_between(0,1,CC),
-    FORWARD is AA,
-    REVERSE is 0,
-    LEFT is AA,
-    RIGHT is BB,
-    BOOM is CC,
-    MSG = "Regra padrao aplicada".
+% obter_controles(INFORMACAO, ADVERSARIOS, MISSEIS, CONTROLES) :-
+%     INFORMACAO = [X, Y, ANGLE, SCORE, SPEED],
+%     CONTROLES = [FORWARD, REVERSE, LEFT, RIGHT, BOOM, MSG],
+%     random_between(0,1,AA),
+%     troca(AA, BB),
+%     random_between(0,1,CC),
+%     FORWARD is AA,
+%     REVERSE is 0,
+%     LEFT is AA,
+%     RIGHT is BB,
+%     BOOM is CC,
+%     MSG = "Regra padrao aplicada".
     %opcao:
     %term_string([ADVERSARIOS| [MISSEIS]], MSG).
 
@@ -52,3 +52,38 @@ obter_controles(_, _, _, [0,0,0,0,0,"nenhuma regra aplicada"]).
 % Ex.:
 % ?- term_string([1,2,"teste",oi], MSG.
 % MSG = "[1,2,\"teste\",oi]".
+
+obter_controles(IFORMACAO, ADVERSARIOS, MISSEIS, CONTROLES) :-
+    INFORMACAO = [X, Y, ANGLE, SCORE, SPEED],
+    CONTROLES = [FORWARD, REVERSE, LEFT, RIGHT, BOOM, MSG].
+
+
+% Regra: Define se o jato está em perigo.
+perigo(INFORMACAO, ADVERSARIOS, MISSEIS) :-
+    INFORMACAO = [X, Y, _ANGLE, _SCORE, _SPEED],
+    missil_perto([X, Y], MISSEIS). 
+
+perigo(INFORMACAO, ADVERSARIOS, MISSEIS) :-
+    INFORMACAO = [X, Y, _ANGLE, _SCORE, _SPEED],
+    adversario_perto([X, Y], ADVERSARIOS).
+
+% Regra: Encontra o primeiro míssel dentro de um raio de distância.
+missil_perto(COORDENADAS, MISSEIS, [MX, MY]) :-
+    COORDENADAS = [X, Y],
+    member([MX, MY], MISSEIS),
+    distancia([X, Y], [MX, MY], D),
+    D =< 50.
+
+% Regra: Encontra o primeiro adversário dentro de um raio de distância.
+adversario_perto(COORDENADAS, ADVERSARIOS, [AX, AY]) :-
+    COORDENADAS = [X, Y],
+    member([AX, AY], ADVERSARIOS),
+    distancia([X, Y], [AX, AY], D),
+    D =< 50.
+
+% Regra: Calcula a distância entre dois pontos.
+distancia([X1, Y1], [X2, Y2], D) :-
+    DX is X2 - X1,
+    DY is Y2 - Y1,
+    D is sqrt(DX * DX + DY * DY).
+
